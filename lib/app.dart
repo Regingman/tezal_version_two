a231f5b2b5cd45acad6678220ea341de07b36386
Атай <51788708+Regingman@users.noreply.github.com>
2020-07-13 08:25:39 +0600

    init

---
+++ lib/app.dart
@@ -0,0 +1,40 @@
+import 'package:first_time_screen/first_time_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:turna_tv/blocs/movie_detail_bloc/movie_detail_bloc.dart';
+
+import 'app_config.dart';
+import 'blocs/auth_bloc/auth_bloc.dart';
+import 'blocs/category_bloc/category_bloc.dart';
+import 'blocs/home_bloc/home_bloc.dart';
+import 'blocs/registration_bloc/registration_bloc.dart';
+import 'blocs/search_bloc/search_bloc.dart';
+import 'data/repositories/repository.dart';
+import 'ui/screens/intro/intro_screen.dart';
+import 'ui/screens/main/main_screen.dart';
+import 'ui/widgets/splash_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => Repository(),
+      child: MultiBlocProvider(
+        providers: [],
+        child: MaterialApp(
+          theme: getTheme(),
+          home: FirstTimeScreen(
+            loadingScreen: SplashScreen(),
+            introScreen: MaterialPageRoute(
+              builder: (context) => IntroScreen(),
+            ),
+            landingScreen: MaterialPageRoute(
+              builder: (context) => MainScreen(),
+            ),
+          ),
+          routes: getRoutes(),
+        ),
+      ),
+    );
+  }
+}
